dea7de49bb26a946c8080f72a9ac3d081faf8fe8
Leon Nicolhas <leonnhsalas@gmail.com>
2023-12-11 10:11:59 -0600

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-numero = int(input("Cual es el numero para dividir"))
+numero = int(input("Cual es el numero para dividir "))
+
+checador = numero % 2
+
+if numero == 0:
+    print("No es ninguno")
+elif numero == 2 or numero == -2:
+    print("numero par y primo")
+elif checador == 0:
+    print("numero par")
+else:
+    print("numero primo")
